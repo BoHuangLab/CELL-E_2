bd9fb15be6ec0662f24052532b0d0440bf93d792
Aaron Agarunov <agarunov.aaron@gmail.com>
2023-09-28 13:30:25 -0400

    Use vercel's example for Next.js GH pages

---
+++ out/_next/static/chunks/pages/_app-00af455fb99aee7f.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{695:function(r,n,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return e(3147)}])},3147:function(r,n,e){"use strict";e.r(n),e.d(n,{default:function(){return o}});var t=e(8650);function o(r){let{Component:n,pageProps:e}=r;return(0,t.jsx)(n,{...e})}e(7910)},7910:function(){},4794:function(r,n,e){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var t=e(9916),o=Symbol.for("react.element"),u=Symbol.for("react.fragment"),f=Object.prototype.hasOwnProperty,_=t.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,c={key:!0,ref:!0,__self:!0,__source:!0};function i(r,n,e){var t,u={},i=null,s=null;for(t in void 0!==e&&(i=""+e),void 0!==n.key&&(i=""+n.key),void 0!==n.ref&&(s=n.ref),n)f.call(n,t)&&!c.hasOwnProperty(t)&&(u[t]=n[t]);if(r&&r.defaultProps)for(t in n=r.defaultProps)void 0===u[t]&&(u[t]=n[t]);return{$$typeof:o,type:r,key:i,ref:s,props:u,_owner:_.current}}n.Fragment=u,n.jsx=i,n.jsxs=i},8650:function(r,n,e){"use strict";r.exports=e(4794)}},function(r){var n=function(n){return r(r.s=n)};r.O(0,[179],function(){return n(695),n(2717)}),_N_E=r.O()}]);
